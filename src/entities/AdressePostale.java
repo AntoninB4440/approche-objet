127d33c1fc3caede6983e9357e2b14ae205ae618
AntoninB4440 <barceloantonin@gmail.com>
2021-01-27 11:43:41 +0100

    Ajout consigne TP2

--- src/entities/AdressePostale.java
+++ src/entities/AdressePostale.java
@@ -6,5 +6,24 @@ public class AdressePostale {
 	public String nomRue;
 	public String codePostal;
 	public String ville;
+	
+	public AdressePostale(){
+		numeroRue = 128;
+		nomRue = "Impasse des Nympheas";
+		codePostal = "44300";
+		ville = "Nantes";
+	}
+	
+	public AdressePostale(int numeroRue, String nomRue, String codePostal, String ville){
+		this.numeroRue = numeroRue;
+		this.nomRue = nomRue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
 
 }
+
+
+
+
+
--- src/essais/TestAdressePostale.java
+++ src/essais/TestAdressePostale.java
@@ -7,6 +7,7 @@ public class TestAdressePostale {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+			/*//Methode sans constructor 
 			AdressePostale adresse1 = new AdressePostale();
 			adresse1.numeroRue = 128;
 			adresse1.nomRue = "Impasse des Nymphéas";
@@ -18,6 +19,13 @@ public class TestAdressePostale {
 			adresse2.nomRue = "Boulevard Jules Verne";
 			adresse2.codePostal = "40320";
 			adresse2.ville = "Buanes";
+			*/
+		
+			//Construteur vide
+			AdressePostale adresse1 = new AdressePostale();
+			
+			//Construteur avec paramètre
+			AdressePostale adresse2 = new AdressePostale(114,"Boulevard Jules Vernes","44300","Nantes");
 			
 			
 	}
