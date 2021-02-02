f57146516eecea3179428778047290111227b519
AntoninB4440 <barceloantonin@gmail.com>
2021-02-02 11:22:59 +0100

    CréationList tp

---
+++ src/listes/CreationList.java
@@ -0,0 +1,19 @@
+package listes;
+
+import java.util.ArrayList;
+
+public class CreationList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		for (int i = 1; i <= 100; i++) {
+			arrayList.add(i);
+		}
+
+		System.out.println("La taille de la list arrayList est de : " + arrayList.size() + " elements");
+
+	}
+
+}
